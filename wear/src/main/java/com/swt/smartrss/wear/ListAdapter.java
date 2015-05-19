83665bcbaf6420bcdf44e380addd437c70b04dac
Florian Lüdiger <florian.luediger@googlemail.com>
2015-05-19 16:09:28 +0200

    Created and formatted wearable ListView for the feed list.

---
+++ wear/src/main/java/com/swt/smartrss/wear/ListAdapter.java
@@ -0,0 +1,54 @@
+package com.swt.smartrss.wear;
+
+import android.content.Context;
+import android.support.wearable.view.WearableListView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Florian on 19.05.2015.
+ */
+public class ListAdapter extends WearableListView.Adapter {
+    private String[] mDataset;
+    private Context mContext;
+    private LayoutInflater mInflater;
+
+    public ListAdapter(Context context, String[] dataset) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mDataset = dataset;
+    }
+
+    public class ItemViewHolder extends WearableListView.ViewHolder {
+        TextView textView;
+        //ImageView imageView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+
+            textView = (TextView) itemView.findViewById(R.id.textView);
+            //imageView = (ImageView) imageView.findViewById(R.id.imageView);
+        }
+    }
+
+    public WearableListView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        return new ItemViewHolder(mInflater.inflate(R.layout.listview_entry, null));
+    }
+
+    @Override
+    public void onBindViewHolder(WearableListView.ViewHolder holder, int position) {
+        ItemViewHolder itemHolder = (ItemViewHolder) holder;
+        TextView view = itemHolder.textView;
+
+        view.setText(mDataset[position]);
+        holder.itemView.setTag(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
--- wear/src/main/java/com/swt/smartrss/wear/MainActivity.java
+++ wear/src/main/java/com/swt/smartrss/wear/MainActivity.java
@@ -3,22 +3,41 @@ package com.swt.smartrss.wear;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.WearableListView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements WearableListView.ClickListener {
 
     private TextView mTextView;
+    private WearableListView listView;
+    private String testStrings[] = {"Affe","Banane","Chameleon","Drogen","Was passiert eigentlich, wenn ich hier einen längeren String eintrage?"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        setContentView(R.layout.round_activity_main);
+
+        listView = (WearableListView) findViewById(R.id.list);
+        listView.setClickListener(this);
+
+        listView.setAdapter(new ListAdapter(this, testStrings));
+
+        /*final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                //mTextView = (TextView) stub.findViewById(R.id.text);
             }
-        });
+        });*/
+    }
+
+    @Override
+    public void onClick(WearableListView.ViewHolder viewHolder) {
+
+    }
+
+    @Override
+    public void onTopEmptyRegionClick() {
+
     }
 }
