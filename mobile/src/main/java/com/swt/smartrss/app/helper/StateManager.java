84ee532e02061802f81d7e82554658c093d0be96
Dropsoft <ok>
2015-06-01 17:43:34 +0200

    added global state

---
+++ mobile/src/main/java/com/swt/smartrss/app/helper/StateManager.java
@@ -0,0 +1,14 @@
+package com.swt.smartrss.app.helper;
+
+import android.content.Context;
+
+/**
+ * Created by Dropsoft on 01.06.2015.
+ */
+public class StateManager {
+    Context mContext;
+
+    public StateManager(Context context) {
+        this.mContext = context;
+    }
+}
