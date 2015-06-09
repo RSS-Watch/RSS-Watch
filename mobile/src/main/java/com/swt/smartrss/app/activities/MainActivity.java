12f3dafbcff728921b16b2a27e8c6ae0499ef60c
Florian Lüdiger <florian.luediger@googlemail.com>
2015-06-09 14:41:03 +0200

    Created reader activity that shows title and text of the selected article

--- mobile/src/main/java/com/swt/smartrss/app/activities/MainActivity.java
+++ mobile/src/main/java/com/swt/smartrss/app/activities/MainActivity.java
@@ -1,15 +1,17 @@
-package com.swt.smartrss.app;
+package com.swt.smartrss.app.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+import com.swt.smartrss.app.GlobalApplication;
+import com.swt.smartrss.app.R;
 import com.swt.smartrss.app.helper.StateManager;
 import com.swt.smartrss.app.adapters.ListAdapter;
 import org.feedlyapi.FeedManager;
@@ -20,6 +22,7 @@ import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,15 +40,25 @@ public class MainActivity extends Activity {
 
         final ListView listView = (ListView) findViewById(R.id.listView);
 
-        //final ArrayList<String> list = new ArrayList<String>();
-        //list.add("loading");
-
         final ArrayList<Article> list = new ArrayList<Article>();
         list.add(new Article());
 
-        //final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, list);
         final ListAdapter adapter = new ListAdapter(this,android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), ReaderActivity.class);
+                Article current = adapter.getItem(position);
+                i.putExtra("title", adapter.getItem(position).getTitle());
+                if(current.getContent() != null && !current.getContent().isEmpty())
+                    i.putExtra("text",adapter.getItem(position).getContent());
+                else
+                    i.putExtra("text",adapter.getItem(position).getSummary());
+                startActivity(i);
+            }
+        });
 
         FeedManager feedManager = new FeedManager(FeedlyApiProvider.getApi(stateManager.getAndroidPreferences().getFeedlyToken()));
         feedManager.getLatestArticles(10, new Callback<Stream>() {
@@ -53,18 +66,12 @@ public class MainActivity extends Activity {
             public void success(Stream stream, Response response) {
                 List<Article> articles = stream.getItems();
                 list.clear();
-                //for (Article a: articles) {
-                //    list.add(a.getTitle());
-                //}
                 list.addAll(articles);
                 adapter.notifyDataSetChanged();
             }
 
             @Override
             public void failure(RetrofitError retrofitError) {
-                //list.clear();
-                //list.add("error");
-                //adapter.notifyDataSetChanged();
                 retrofitError.printStackTrace();
             }
         });
---
+++ mobile/src/main/java/com/swt/smartrss/app/activities/ReaderActivity.java
@@ -0,0 +1,50 @@
+package com.swt.smartrss.app.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import com.swt.smartrss.app.R;
+
+public class ReaderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reader);
+        TextView textViewTitle = (TextView)findViewById(R.id.textViewTitle);
+        TextView textViewText = (TextView) findViewById(R.id.textViewText);
+
+        Intent i = getIntent();
+        String title = i.getStringExtra("title");
+        String text = i.getStringExtra("text");
+
+        textViewTitle.setText(title);
+        textViewText.setText(text);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_reader, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
