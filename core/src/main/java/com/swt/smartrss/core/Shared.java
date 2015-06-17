7c3cc879c7cf0d32e370f4b417bcec4ee9161404
Dropsoft <ok>
2015-06-17 22:30:05 +0200

    added  shared const

---
+++ core/src/main/java/com/swt/smartrss/core/Shared.java
@@ -0,0 +1,13 @@
+package com.swt.smartrss.core;
+
+/**
+ * Created by Dropsoft on 17.06.2015.
+ */
+public class Shared {
+    public static final String URI_REQUEST_ARTICLE_LIST = "/reqArticleList";
+    public static final String URI_REQUEST_ARTICLE_TEXT = "/reqArticleText";
+    public static final String URI_OPEN_ARTICLE = "/openArticle";
+    public static final String URI_ARTICLE = "/article";
+    public static final String KEY_TIME = "time";
+    public static final String KEY_DATA = "data";
+}
